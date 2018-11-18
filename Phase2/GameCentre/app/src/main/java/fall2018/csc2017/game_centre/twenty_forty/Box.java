f2aafbbd54e0e60cabd5c4edd250fb2cc83b0f74
Grace Hu <gracek.hu@mail.utoronto.ca>
2018-11-17 21:34:08 -0500

    Added attribute exponent to Box class.

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/game_centre/twenty_forty/Box.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/game_centre/twenty_forty/Box.java
@@ -1,6 +1,25 @@
 package fall2018.csc2017.game_centre.twenty_forty;
 
+import java.util.Objects;
+
 public class Box {
-    //TODO: Basically the Tiles of 2048.
+    private int exponent;
     //TODO: Need to make power of 2s tiles: 2,4,8,16,32,64,.... (Photoshop time)
+
+    Box(int exponent) {
+        this.exponent = exponent;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        else if (o == null || getClass() != o.getClass()) return false;
+        Box box = (Box) o;
+        return exponent == box.exponent;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(exponent);
+    }
 }
