df1f1ac5f4e10e6272f7d8d99615c0e5b51178f8
Ethan <ethan.akester@gmail.com>
2018-11-29 22:57:27 -0800

    Added beginning of a Darkmode button to gamemanager

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/game_centre/GameoverActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/game_centre/GameoverActivity.java
@@ -2,27 +2,19 @@ package fall2018.csc2017.game_centre;
 
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import fall2018.csc2017.game_centre.minesweeper.MinesweeperGame;
+public class GameoverActivity extends AppCompatActivity {
 
-public class GameoverActivity extends AppCompatActivity{
-
-    private Button gameoverButton;
+     Button gameoverButton;
     private String currentUsername;
     private String gameFilename;
     private Game game;
 
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameover);
         addGameoverButton();
@@ -31,16 +23,17 @@ public class GameoverActivity extends AppCompatActivity{
         game = (Game) getIntent().getSerializableExtra("GAME");
     }
 
-    public void addGameoverButton(){
+    public void addGameoverButton() {
         gameoverButton = findViewById(R.id.gameoverButton);
         gameoverButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 switchToGMA();
-            }});
+            }
+        });
     }
 
-    public void switchToGMA(){
+    public void switchToGMA() {
         Intent GMAIntent = new Intent(getApplicationContext(), GameMenuActivity.class);
         Bundle gmaBundle = new Bundle();
         gmaBundle.putSerializable("GAME", game);
