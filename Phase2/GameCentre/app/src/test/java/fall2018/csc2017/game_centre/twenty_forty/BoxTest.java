854302a8c63a92224c700001469eaf66adfe680e
MaXeraph <33969759+MaXeraph@users.noreply.github.com>
2018-11-30 00:18:26 -0500

    JavaDoc for BoxTest

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_centre/twenty_forty/BoxTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_centre/twenty_forty/BoxTest.java
@@ -1,5 +1,58 @@
+package fall2018.csc2017.game_centre.twenty_forty;
+
+import org.junit.Test;
+
+import fall2018.csc2017.game_centre.R;
+
 import static org.junit.Assert.*;
 
 public class BoxTest {
 
+    /**
+     * Test whether isEmptyBox works
+     */
+    @Test
+    public void isEmptyBox() {
+        Box box = new Box(0);
+        assertTrue(box.isEmptyBox());
+    }
+
+    /**
+     * Test whether getBackground works
+     */
+    @Test
+    public void getBackground() {
+        Box box = new Box(1);
+        assertEquals(box.getBackground(), R.drawable.box_2);
+    }
+
+    /**
+     * Test whether equals works
+     */
+    @Test
+    public void equals() {
+        Box box1 = new Box(1);
+        Box box2 = new Box(1);
+        assertEquals(box1, box2);
+    }
+
+    /**
+     * Test whether hashCode works
+     */
+    @Test
+    public void hash(){
+        Box box1 = new Box(1);
+        Box box2 = new Box(1);
+        assertEquals(box1.hashCode(), box2.hashCode());
+    }
+
+    /**
+     * Test whether setExponent works
+     */
+    @Test
+    public void setExponent(){
+        Box box = new Box(1);
+        box.setExponent(2);
+        assertEquals(2,box.getExponent());
+    }
 }
